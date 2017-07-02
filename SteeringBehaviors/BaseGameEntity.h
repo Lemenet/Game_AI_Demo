e8aca73095387d1f93b169ea145b455baefc58ba
Lemenet <lemenet@live.com>
2017-07-02 10:32:24 +0800

    完成了BaseGameEntity类
    补全BaseGameEntity.h中所有的内容，完成基类的实现。

--- SteeringBehaviors/BaseGameEntity.h
+++ SteeringBehaviors/BaseGameEntity.h
@@ -7,7 +7,7 @@
 //  Desc: Base class to define a common interface for all game
 //        entities
 //
-//  Author: haungmegndi 2017
+//  Author: HaungMengdi 2017
 //
 //------------------------------------------------------------------------
 
@@ -93,5 +93,71 @@ protected:
 		bTag_(false)
 	{}
 
+	//this can be used to create an entity with a 'forced' ID. It can be used
+	//when a previously created entity has been removed and deleted from the
+	//game for some reason. For example, The Raven map editor uses this ctor 
+	//in its undo/redo operations. 
+	//USE WITH CAUTION!
+
+	BaseGameEntity(int entityType, int ForcedID) :
+		ID_(ForcedID),
+		boundingRadius_(0.0),
+		position_(Vector2D()),
+		scale_(Vector2D(1.0, 1.0)),
+		entityType_(entityType),
+		bTag_(false)
+	{}
+
+
+public:
+
+	virtual ~BaseGameEntity() {}
+
+	virtual void Update(double deltaTime) {}
+
+	virtual void Render() {}
+
+	virtual bool HandleMessage(const Telegram& msg) { return false; }
+
+	//Entity should be able to read and write their data to a stream
+	virtual void WriteData(std::ostream& os) const {}
+	virtual void ReadData(std::ifstream& is) {}
+
+
+public:
+	//----------Interface----------
+
+	//position_
+	Vector2D GetPosition() const { return position_; }
+	void SetPosition(const Vector2D& newPosition) { position_ = newPosition; }
+
+	//boundingRadius_
+	double GetRadius() const { return boundingRadius_; }
+	void SetRadius(double r) { boundingRadius_ = r; }
+	
+	//ID_
+	int GetID() const { return ID_; }
+
+	//bTag_
+	bool IsTagged() const { return bTag_; }
+	void DoTag() { bTag_ = true; }
+	void UnTag() { bTag_ = false; }
+
+	//Scale_
+	Vector2D GetScale() const { return scale_; }
+	void SetScale(const Vector2D& val)
+	{
+		boundingRadius_ *= MaxOf(val.x, val.y) / MaxOf(scale_.x, scale_.y);
+		scale_ = val;
+	}
+	void SetScale(double val)
+	{
+		boundingRadius_ *= (val / MaxOf(scale_.x, scale_.y));
+		scale_ = Vector2D(val, val);
+	}
+
+	//Entity type
+	int GetEntityType() const { return entityType_; }
+	void SetEntityType(int newType) { entityType_ = newType; }
 
 };
